9fa3ad126ea8cc05672402aa5a891a2db9e40352
Robin Giles Ribera <robinchogiles@gmail.com>
2019-01-13 16:50:16 +0100

    Add config for firebase

---
+++ recursosw/src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  // Initialize Firebase
+  apiKey: 'AIzaSyA_pdyr1w6IeX7gVfZdWZE7YUFt6ooTcmE',
+  authDomain: 'recursosw-33905.firebaseapp.com',
+  databaseURL: 'https://recursosw-33905.firebaseio.com',
+  projectId: 'recursosw-33905',
+  storageBucket: 'recursosw-33905.appspot.com',
+  messagingSenderId: '423094724158'
+}
---
+++ recursosw/src/config/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/auth'
+import fbSetttings from './firebase'
+
+const firebaseApp = firebase.initializeApp(fbSetttings)
+
+export const auth = firebaseApp.auth()
+export const db = firebaseApp.firestore()
+export const storage = firebaseApp.storage()
+export const ghProvider = new firebase.auth.GithubAuthProvider()
+db.settings({ timestampsInSnapshots: true })
