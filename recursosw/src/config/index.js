4c996a7d340ceb4b5c25dff694398c8a1bdfe5bf
Robin Giles Ribera <robinchogiles@gmail.com>
2019-01-13 15:43:12 +0100

    Add config for firebsae

---
+++ recursosw/src/config/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/auth'
+import fbSetttings from './firebase'
+
+const firebaseApp = firebase.initializeApp(fbSetttings)
+
+export const auth = firebaseApp.auth()
+export const db = firebaseApp.firestore()
+export const storage = firebaseApp.storage()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+export const ghProvider = new firebase.auth.GithubAuthProvider()
+db.settings({ timestampsInSnapshots: true })
