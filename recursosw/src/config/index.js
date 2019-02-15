a48f174a7601352bf70df5e20afcda6e6613a2ce
Robin Giles Ribera <robinchogiles@gmail.com>
2019-02-15 14:40:04 +0100

    Comentar las cosillas

--- recursosw/src/config/index.js
+++ recursosw/src/config/index.js
@@ -1,13 +1,4 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
-import 'firebase/storage'
-import 'firebase/auth'
-import fbSetttings from './firebase'
+// importar firebase (firestore, storage, auth)
 
-const firebaseApp = firebase.initializeApp(fbSetttings)
-
-export const auth = firebaseApp.auth()
-export const db = firebaseApp.firestore()
-export const storage = firebaseApp.storage()
-export const ghProvider = new firebase.auth.GithubAuthProvider()
-db.settings({ timestampsInSnapshots: true })
+// exportar los servicios de firebase:
+// auth, db, storage, ghProvider
