0e56d515c344f2e41901f5350561c28060758376
Robin Giles Ribera <robinchogiles@gmail.com>
2019-02-16 00:15:48 +0100

    clean index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,24 +7,14 @@ const fs = require('fs-extra');
 
 admin.initializeApp()
 
-const formats: any = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/webp': 'webp'
-}
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+//  response.end();
+// });
 
-const compressionOptions: any = {
-  'image/png': {compressionLevel: 6},
-  'image/jpeg': {quality: 60},
-  'image/webp': {quality: 60}
-}
-
-const getDownloadUrl = function (bucket: any, object: any, destination: string) {
-  return `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/`
-    + encodeURIComponent(destination)
-    + '?alt=media&token='
-    + object.metadata.firebaseStorageDownloadTokens
-}
 
 export const generateThumbs = functions.storage
   .object()
@@ -37,58 +27,21 @@ export const generateThumbs = functions.storage
     const workingDir = join(tmpdir(), 'thumbs')
     const tmpFilePath: any = join(workingDir, `source${new Date().getTime()}.png`)
 
-    const contentType: any = object.contentType
-    const format = formats[contentType]
-    const compression = compressionOptions[contentType]
+    // const contentType: any = object.contentType
+    // const format = formats[contentType]
+    // const compression = compressionOptions[contentType]
 
-    // Para no producir un bucle infinito
-    if (fileName.includes('thumb@') || !contentType.includes('image')) {
-      console.log('exiting function')
-      return false
-    }
+    // 0. Avoid infinite loop
 
     // 1. Ensure thumbnail dir exists
-    await fs.ensureDir(workingDir)
 
     // 2. Download Source File
-    await bucket.file(filePath).download({
-      destination: tmpFilePath
-    })
 
     // 3. Resize the images and define an array of upload promises
-    const thumbs: any = {}
-    const sizes = [[128, 128], [340, 340], [640, 640]]
-
-    const promises = sizes.map(async size => {
-      const [w, h] = size
-      const thumbName = `thumb@${size[0]}_${fileName}`
-      const thumbPath = join(workingDir, thumbName)
-
-      // resize image
-      await sharp(tmpFilePath)
-        .resize(w, h)[format](compression)
-        .toFile(thumbPath)
-
-      const destination = join(bucketDir, thumbName)
-      const imgUrl = getDownloadUrl(bucket, object, destination)
-
-      thumbs[size.join('x')] = imgUrl
-
-      // Upload to GCS
-      return bucket.upload(thumbPath, {destination})
-    })
 
     // 4. Run the upload operations
-    await Promise.all(promises)
 
     // 5. Cleanup remove the tmp/thumbs from the filesystem
-    await fs.remove(workingDir)
 
     // 6. set thumbs to firestore
-    thumbs.mainImg = getDownloadUrl(bucket, object, filePath)
-    return admin.firestore().doc(`/${ref}/${resourceId}`).set({
-      media: thumbs,
-      photoRef: filePath,
-    }, {merge: true})
-    .catch(error => console.log({thumbs: error}))
   })
